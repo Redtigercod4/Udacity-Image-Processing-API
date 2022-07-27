bb6a734be9f10a3b0861d6f556082d13bf4ab2e1
Wesley Whittle <wesleywhittle@hotmail.co.uk>
2022-07-27 18:41:58 +0100

    Jasmine Unit Test Setup
    Setting up jasmine to run the unit tests once compiled. Contains a basic test to confirm correct configuration.

---
+++ build/tests/index.spec.js
@@ -0,0 +1,6 @@
+"use strict";
+describe('Basic Test to check that jasmine is configured correctly as per Udacity course', () => {
+    it('Checks if true is equal to true', () => {
+        expect(true).toEqual(true);
+    });
+});
---
+++ src/tests/index.spec.ts
@@ -0,0 +1,5 @@
+describe('Basic Test to check that jasmine is configured correctly as per Udacity course', () => {
+    it('Checks if true is equal to true', () => {
+        expect(true).toEqual(true);
+    });
+});
