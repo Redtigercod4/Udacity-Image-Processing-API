c0af14dfd6833351789f403408acd6fbc77f7360
Wes <76014409+Redtigercod4@users.noreply.github.com>
2022-07-27 18:43:04 +0100

    Delete build directory

--- build/tests/index.spec.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-describe('Basic Test to check that jasmine is configured correctly as per Udacity course', () => {
-    it('Checks if true is equal to true', () => {
-        expect(true).toEqual(true);
-    });
-});
