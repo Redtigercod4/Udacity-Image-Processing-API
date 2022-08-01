4ab83e6c56687f06f4fc6a60a256757b2433bb34
redtigercod4 <wesleywhittle@hotmail.co.uk>
2022-08-01 21:24:09 +0100

    Conversion of file and saving to output

--- src/components/processor.ts
+++ src/components/processor.ts
@@ -1,12 +1,11 @@
 import sharp from "sharp";
-import path, { dirname } from "path";
+import path from "path";
 import fs from 'fs';
 
 function processor(image: string, width: number, height: number) {
-  const activePath = `${path.resolve(`${__dirname}/assets/${image}.jpg`)}`
-  const outputPath = `${path.resolve(`${__dirname}/components/output/${image}_${width}x${height}`)}`
+  const activePath = `${path.resolve(`./src/assets/${image}.jpg`)}`
   try {
-    sharp(activePath).resize(width, height)
+    sharp(activePath).resize(width, height).toFile(`./src/assets/output/${image}_${width}x${height}.jpg`)
   } catch (error) {
     console.error(error);
   }
