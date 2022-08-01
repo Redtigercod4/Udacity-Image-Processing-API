14c82083da51a6be6d99b1324d030f7fcc998ebc
redtigercod4 <wesleywhittle@hotmail.co.uk>
2022-08-01 20:48:01 +0100

    Conversion to size

--- src/components/processor.ts
+++ src/components/processor.ts
@@ -1,16 +1,12 @@
 import sharp from "sharp";
-import path from "path";
+import path, { dirname } from "path";
 import fs from 'fs';
 
 function processor(image: string, width: number, height: number) {
-  const imagePath = () => {
-    const sourcePath = path.resolve(__dirname, "./assets");
-    const imagePath = path.resolve(sourcePath, `${image}.jpg`);
-    return imagePath;
-  };
-
+  const activePath = `${path.resolve(`${__dirname}/assets/${image}.jpg`)}`
+  const outputPath = `${path.resolve(`${__dirname}/components/output/${image}_${width}x${height}`)}`
   try {
-    sharp(imagePath()).resize(width, height).toFile('output.png')
+    sharp(activePath).resize(width, height)
   } catch (error) {
     console.error(error);
   }
